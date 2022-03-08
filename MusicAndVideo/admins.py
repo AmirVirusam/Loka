2637912093d5810e88a8e9031a49ba2194eee1fc
MrVirus <88379683+AmirVirusam@users.noreply.github.com>
2022-03-09 01:58:22 +0330

    Add files via upload

---
+++ MusicAndVideo/admins.py
@@ -0,0 +1,86 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from config import HNDLR, call_py
+from MusicAndVideo.helpers.decorators import authorized_users_only
+from MusicAndVideo.helpers.handlers import skip_current_song, skip_item
+from MusicAndVideo.helpers.queues import QUEUE, clear_queue
+
+@Client.on_message(filters.command(["skip"], prefixes=f"{HNDLR}"))
+@authorized_users_only
+async def skip(client, m: Message):
+    await m.delete()
+    chat_id = m.chat.id
+    if len(m.command) < 2:
+        op = await skip_current_song(chat_id)
+        if op == 0:
+            await m.reply("**❌ There Is Nothing In The Queue To Skip . . . !**")
+        elif op == 1:
+            await m.reply("**⚡Empty Queue, Leaving Voice Chat . . . !**")
+        else:
+            await m.reply(
+                f"**⏭ Skip Play Back\n🎧 Now Playing** **-** **[{op[0]}]({op[1]})** **|** **{op[2]}** **. . . !**",
+                disable_web_page_preview=True,
+            )
+    else:
+        skip = m.text.split(None, 1)[1]
+        OP = "**🗑️ Removed the following songs from the Queue : -**"
+        if chat_id in QUEUE:
+            items = [int(x) for x in skip.split(" ") if x.isdigit()]
+            items.sort(reverse=True)
+            for x in items:
+                if x == 0:
+                    pass
+                else:
+                    hm = await skip_item(chat_id, x)
+                    if hm == 0:
+                        pass
+                    else:
+                        OP = OP + "\n" + f"**#{x}** **-** **{hm}**"
+            await m.reply(OP)
+
+@Client.on_message(filters.command(["end", "stop"], prefixes=f"{HNDLR}"))
+@authorized_users_only
+async def stop(client, m: Message):
+    await m.delete()
+    chat_id = m.chat.id
+    if chat_id in QUEUE:
+        try:
+            await call_py.leave_group_call(chat_id)
+            clear_queue(chat_id)
+            await m.reply("**✅ End Play Back . . . !**")
+        except Exception as e:
+            await m.reply(f"**ERROR** \n`{e}`")
+    else:
+        await m.reply("**❌ Nothing Is Playing . . . !**")
+
+@Client.on_message(filters.command(["pause"], prefixes=f"{HNDLR}"))
+@authorized_users_only
+async def pause(client, m: Message):
+    await m.delete()
+    chat_id = m.chat.id
+    if chat_id in QUEUE:
+        try:
+            await call_py.pause_stream(chat_id)
+            await m.reply(
+                f"**⏸ Play Back Is Paused !**\n\n**• To Resume Play Back, Use The Command »** `{HNDLR}resume`"
+            )
+        except Exception as e:
+            await m.reply(f"**ERROR** \n`{e}`")
+    else:
+        await m.reply("**❌ Nothing Is Playing . . . !**")
+
+@Client.on_message(filters.command(["resume"], prefixes=f"{HNDLR}"))
+@authorized_users_only
+async def resume(client, m: Message):
+    await m.delete()
+    chat_id = m.chat.id
+    if chat_id in QUEUE:
+        try:
+            await call_py.resume_stream(chat_id)
+            await m.reply(
+                f"**▶ Resume Paused Play Back**\n\n**• To Pause Play Back, Use The Command »** `{HNDLR}pause`**"
+            )
+        except Exception as e:
+            await m.reply(f"**ERROR** \n`{e}`")
+    else:
+        await m.reply("**❌ Nothing Is Playing . . . !**")
---
+++ MusicAndVideo/userbot.py
@@ -0,0 +1,84 @@
+import os
+import sys
+from datetime import datetime
+from time import time
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from config import HNDLR, SUDO_USERS
+
+# System Uptime
+START_TIME = datetime.utcnow()
+TIME_DURATION_UNITS = (
+    ("Year", 60 * 60 * 24 * 7),
+    ("Week", 60 * 60 * 24),
+    ("Day", 60 * 60),
+    ("Minutes", 60),
+    ("Seconds", 1),
+)
+
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return "inf"
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append("{} {}{}".format(amount, unit, "" if amount == 1 else ""))
+    return ", ".join(parts)
+
+@Client.on_message(filters.command(["ping"], prefixes=f"{HNDLR}"))
+async def ping(client, m: Message):
+    await m.delete()
+    start = time()
+    current_time = datetime.utcnow()
+    m_reply = await m.reply_text("⚡")
+    delta_ping = time() - start
+    uptime_sec = (current_time - START_TIME).total_seconds()
+    uptime = await _human_time_duration(int(uptime_sec))
+    await m_reply.edit(
+        f"<b>⚡ Ping</b> **{delta_ping * 1000:.3f} ms** \n<b>☄ UpTime</b> **-** **{uptime}**"
+    )
+
+@Client.on_message(
+    filters.user(SUDO_USERS) & filters.command(["restart"], prefixes=f"{HNDLR}")
+)
+async def restart(client, m: Message):
+    await m.delete()
+    loli = await m.reply("1")
+    await loli.edit("2")
+    await loli.edit("3")
+    await loli.edit("4")
+    await loli.edit("5")
+    await loli.edit("6")
+    await loli.edit("7")
+    await loli.edit("8")
+    await loli.edit("9")
+    await loli.edit("**✅ VC Player Restarted . . . !**")
+    os.execl(sys.executable, sys.executable, *sys.argv)
+    quit()
+
+
+@Client.on_message(filters.command(["help"], prefixes=f"{HNDLR}"))
+async def help(client, m: Message):
+    await m.delete()
+    HELP = f"""
+<b>👋 Hallo {m.from_user.mention}!
+
+🛠 MENU BANTUAN
+
+⚡ PERINTAH UNTUK SEMUA ORANG
+• {HNDLR}play [judul lagu | link youtube | balas file audio] - untuk memutar lagu
+• {HNDLR}vplay [judul video | link youtube | balas file video] - untuk memutar video
+• {HNDLR}playlist untuk melihat daftar putar
+• {HNDLR}ping - untuk cek status
+• {HNDLR}help - untuk melihat daftar perintah
+
+⚡ PERINTAH UNTUK SEMUA ADMIN
+• {HNDLR}resume - untuk melanjutkan pemutaran lagu atau video
+• {HNDLR}pause - untuk untuk menjeda pemutaran lagu atau video
+• {HNDLR}skip - untuk melewati lagu atau video
+• {HNDLR}end - untuk mengakhiri pemutaran</b>
+"""
+    await m.reply(HELP)

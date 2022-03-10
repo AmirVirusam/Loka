ab29894d6e473c886a38bbf53964f1957380fd70
MrVirus <88379683+AmirVirusam@users.noreply.github.com>
2022-03-11 02:50:00 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
 import asyncio
-
 from pytgcalls import idle
-
 from config import call_py
-
 
 async def main():
     await call_py.start()
     print(
         """
     ------------------
-   | Userbot Started! |
+   | Userbot Started ! |
     ------------------
 """
     )
     await idle()
-    await arq.close()
-
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
